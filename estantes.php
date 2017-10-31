d2465da3658c34d7b1a37a2592434bec987fc9b2
Odin <odinmiguel97@gmail.com>
2017-10-31 17:41:48 -0300

    criação da classe estantes.php

--- estantes.php
+++ estantes.php
@@ -1,74 +1,44 @@
-<html>
-	<head>
+<?php 
 
-		<script src="js/jquery-3.2.1.min.js"></script>
-		<link rel="stylesheet" href="css/bootstrap.css">
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/bootstrap-theme.css">
-		<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-	</head>
+		
+		// $prateleiras = array(
+			
+		// 	"altura" => 39.4,
+		// 	"comprimento" => 58.0,
+		// 	"largura" => 91.2
+		// );
 
-		<script>
-			$(document).ready(function(){
+		// $
+
+		// echo json_encode($prateleiras);
+
+
+		// function CurvaABC($somaDosProdutos, $produtos[]){
+			
+		// 	$x = 0;
+		// 	for($i = 0; $i < count($produtos); $i++ ){
+
+		// 	  $x += ($produtos[$i]->demanda * $produto[$i]->preco);
+		// 	  $somaDosProdutos++;
 				
-			});
-		</script>		
+		// 	}
 
-	<body>	
-		
-		<table class="table">
-	   		<thead>
-	   			<tr>	
-		   			<th>descrição</th>
-		   			<!-- th>demanda anual</th>
-		   			<th>embalagem</th>
-		   			<th>preço p/ caixa</th>
-		   			<th>quantidade p/ caixa</th>
-		   			<th>U.M</th>
-		   			<th>caixas lado a lado</th> -->
-	   			</tr>
-	   		</thead>
-		   		<tbody>
-		   			<tr>
-		   				
-		   			</tr>
-		   		</tbody>
-	   </table>
-		
-	</body>
-	<script>
-	  // Initialize Firebase
-	  var config = {
-	    apiKey: "AIzaSyBuHIAh8eR5UgTKfHAtKaxCfNGo-ntCUKA",
-	    authDomain: "farmacia-86c1d.firebaseapp.com",
-	    databaseURL: "https://farmacia-86c1d.firebaseio.com",
-	    projectId: "farmacia-86c1d",
-	    storageBucket: "farmacia-86c1d.appspot.com",
-	    messagingSenderId: "307955126145"
-	  };
-	  firebase.initializeApp(config);
+		// 	for ($j = 0; $j < count($produtos); $j++) { 
+				
+		// 		$y = $produtos[$j]->preco / $x;
+		// 	}
 
-	  var database = firebase.database();
-	  var url = database.ref();
-	  var urlRef = url.child("Farmacia");
-	  var urlLot = url.child("loteUm");
-	  urlRef.once("value", function(snapshot){
-	  	snapshot.forEach(function(child, i){
-	  		console.log(child.key+": "+child.val().descricao);
-	  		// var sn = snapshot[i].child.val().loteUm.forEach(function(children){
-	  		// 	console.log(children.key+ ": "+ children.val());
-	  		// });
-	  	});
-	  });
+		// }
+
+		$json_file = file_get_contents("farmacia-86c1d-export.json");
+		$json_str = json_decode($json_file, true);
+		$itens = $json_str['nodes'];
+
+		foreach ( $itens['Farmacia'] as $val) { 
+			echo $val['descricao'] ."<br>"; 
+		} 
 
 
-	  urlLot.once("value", function(snapshot){
-	  	snapshot.forEach(function(child, i){
-	  		console.log(child.key+": "+child.val().quantidade);
-	  		// var sn = snapshot[i].child.val().loteUm.forEach(function(children){
-	  		// 	console.log(children.key+ ": "+ children.val());
-	  		// });
-	  	});
-	  });
-	  </script>
-</html>
+
+
+ ?>
