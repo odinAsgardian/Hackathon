69275208552fab9d96b880fa1a5aba004dd24197
Suenesilva <suenesilvasantana@hotmail.com>
2017-10-31 15:35:22 -0300

    cadastro de item

--- add.php
+++ add.php
@@ -2,11 +2,21 @@
     session_start();
 
     $nome = $_POST['nome'];
-    $quantidade = $_POST['quantidade'];
-    $volume = $_POST['volume'];
+    $quantidade_por_caixa = $_POST['quantidade_por_caixa'];
+    $embalagem = $_POST['embalagem'];
+    $largura = $_POST['largura'];
+    $comprimento = $_POST['comprimento'];
+    $altura = $_POST['altura'];
     $preco = $_POST['preco'];
-    
-    array_push($_SESSION['produtos'], [$nome, $quantidade,$volume,$preco]);
+    $demanda_anual = $_POST['demanda_anual'];
+    $lote1 = $_POST['lote1'];
+    $lote2 = $_POST['lote2'];
+    $lote3 = $_POST['lote3'];
+    $quantidade = $_POST['quantidade'];
+    $entrada = $_POST['entrada'];
+    $validade = $_POST['validade'];
+    $venda = $_POST['venda'];
+    array_push($_SESSION['produtos'], [$nome, $quantidade_por_caixa,$embalagem,$largura,$comprimento,$altura,$preco,$demanda_anual,$lote1,$lote2,$lote3,$quantidade,$entrada,$validade,$venda]);
 
     header('location: index.php');
 ?>
