cc0acd8d97d67ba66590f035e748f3b723163282
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-10-30 23:12:02 -0200

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@
 	session_start();
 
 	$id = $_GET['id'];
-	unset($_SESSION['dados'][$id]); // remover algo de um array
+	unset($_SESSION['produtos'][$id]); // remover algo de um array
 
 	header('location: index.php');
 	
